ca2f47b0aaf5abf27f671683128934e1eef07421
Utkarsh <utk1298@gmail.com>
2026-05-22 15:23:32 +0530

    asynchronous update

--- Part_1/react-basics/src/App.jsx
+++ Part_1/react-basics/src/App.jsx
@@ -1,42 +1,36 @@
 import { useState } from "react"
 
-const Display = ({counter})=> {
-  return (
-    <div>{counter}</div>
-  )
-}
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total,setTotal ] = useState(0)
 
-const Button = ({onClick,text}) => {
-  return (
-    <button onClick={onClick}>{text}</button>
-  )
-}
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
+  }
 
-const App = () =>{
-  const [counter , setCounter ] = useState(0)
-  console.log('rendering with counter value', counter)
 
-  const increaseByOne = () => {
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
-  
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updatedRight = right + 1
+    setRight(updatedRight)
+    setTotal(left + updatedRight)
   }
-  const setToZero = () =>{
-    console.log('resetiing to zero , value before', counter)
-    setCounter(0)
-  }
-  const decreaseByOne = () => {
-    console.log('decreasing, value before',counter)
-    setCounter(counter - 1 )
-  }
+
   return (
     <div>
-      <Display counter={counter}/>
-      <Button onClick={increaseByOne} text='plus'/>
-      <Button onClick={setToZero} text='zero'/>
-      <Button onClick={decreaseByOne} text='minus'/>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+
+      <p>{allClicks.join(' ')}</p>
+      <p>total {total}</p>
     </div>
-    
   )
 }
 
