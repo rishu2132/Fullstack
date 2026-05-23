c329136607e84a6eb17f6b01592993292715838d
Utkarsh <utk1298@gmail.com>
2026-05-23 12:22:31 +0530

    exercise 2.6 show numbers from input

--- Part_2/Exercises/Phonebook/src/App.jsx
+++ Part_2/Exercises/Phonebook/src/App.jsx
@@ -6,19 +6,34 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const handleNewNumbers = (event) => {
+   console.log(event.target.value)
+   setNewName(event.target.value)   
+  }
+
+  const addPerson = (event) =>{
+    event.preventDefault()
+    const personObject = {
+      name: newName
+      
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+  
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNewNumbers}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      
+        {persons.map(person => <p key={person.name}>{person.name}</p>)}
     </div>
   )
 }
