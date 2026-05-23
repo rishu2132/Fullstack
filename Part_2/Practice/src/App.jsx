4941de93a555c952f64877093a404ba84dd28c9b
Utkarsh <utk1298@gmail.com>
2026-05-23 11:26:19 +0530

    part 2 b controlled component

--- Part_2/Practice/src/App.jsx
+++ Part_2/Practice/src/App.jsx
@@ -1,7 +1,26 @@
+import { useState } from 'react'
 import Note from './components/Note'
 
-const App = ({notes}) => {
-  
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('a new note...')
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: String(notes.length + 1),
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
 
   return (
     <div>
@@ -9,6 +28,10 @@ const App = ({notes}) => {
       <ul>
         {notes.map(note => <Note key={note.id} note={note} />)}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type='submit'>save</button>
+      </form>
     </div>
   )
 }
