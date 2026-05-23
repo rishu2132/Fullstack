b7f768018095a0ba6dc4f710c9812148e95ed3cc
Utkarsh <utk1298@gmail.com>
2026-05-23 11:46:51 +0530

    2b filtering displayed elements

--- Part_2/Practice/src/App.jsx
+++ Part_2/Practice/src/App.jsx
@@ -4,6 +4,7 @@ import Note from './components/Note'
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
   const [newNote, setNewNote] = useState('a new note...')
+  const [showAll, setShowAll] = useState(true)
 
   const handleNoteChange = (event) => {
     console.log(event.target.value)
@@ -22,11 +23,18 @@ const App = (props) => {
     setNewNote('')
   }
 
+  const notesToShow = showAll ? notes :notes.filter(note => note.important === true)
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important': 'all'}
+        </button>
+      </div>
       <ul>
-        {notes.map(note => <Note key={note.id} note={note} />)}
+        {notesToShow.map(note => <Note key={note.id} note={note} />)}
       </ul>
       <form onSubmit={addNote}>
         <input value={newNote} onChange={handleNoteChange}/>
