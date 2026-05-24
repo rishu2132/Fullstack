223751b6eb874eb7bd095964155d19ba3a2b9723
Utkarsh <utk1298@gmail.com>
2026-05-24 09:00:53 +0530

    send data to server

--- Part_2/Practice/src/App.jsx
+++ Part_2/Practice/src/App.jsx
@@ -31,11 +31,16 @@ const App = () => {
     const noteObject = {
       content: newNote,
       important: Math.random() < 0.5,
-      id: String(notes.length + 1),
     }
 
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    
+    axios
+      .post('http://localhost:3001/notes', noteObject)
+      .then(response => {
+        setNotes(notes.concat(response.data))
+        setNewNote('')
+      })
+    
   }
 
   const notesToShow = showAll ? notes :notes.filter(note => note.important === true)
