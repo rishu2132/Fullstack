e28eead3edc5c8b037217afdf2e9426330d802de
Utkarsh <utk1298@gmail.com>
2026-05-24 10:31:44 +0530

    toggle to change the importance of note

--- Part_2/Practice/src/App.jsx
+++ Part_2/Practice/src/App.jsx
@@ -17,7 +17,16 @@ const App = () => {
       })
   },[])
 
-  
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = {...note, important: !note.important}
+
+    axios.put(url,changedNote).then(response => {
+      setNotes(notes.map(note => note.id === id ? response.data : note))
+    })
+
+  }
 
   console.log('render',notes.length, 'notes')
 
@@ -54,7 +63,7 @@ const App = () => {
         </button>
       </div>
       <ul>
-        {notesToShow.map(note => <Note key={note.id} note={note} />)}
+        {notesToShow.map(note => <Note key={note.id} note={note} toggleImportance={()=> toggleImportanceOf(note.id)}/>)}
       </ul>
       <form onSubmit={addNote}>
         <input value={newNote} onChange={handleNoteChange}/>
