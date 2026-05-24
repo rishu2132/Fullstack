1fcaf25372215adaa19456c8d524bf0274a5389a
Utkarsh <utk1298@gmail.com>
2026-05-24 19:15:43 +0530

    inline CSS and footer Component

--- Part_2/Practice/src/App.jsx
+++ Part_2/Practice/src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect} from 'react'
 import Note from './components/Note'
 import noteService from './services/notes'
 import Notification from './components/Notification'
+import Footer from './components/Footer'
 
 
 const App = () => {
@@ -78,6 +79,7 @@ const App = () => {
         <input value={newNote} onChange={handleNoteChange}/>
         <button type='submit'>save</button>
       </form>
+      <Footer/>
     </div>
   )
 }
