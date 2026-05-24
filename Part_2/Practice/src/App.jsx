232d8609e4d40e209d26de856339ca066fcf8bcc
Utkarsh <utk1298@gmail.com>
2026-05-24 19:08:51 +0530

    add style to heading and error display component

--- Part_2/Practice/src/App.jsx
+++ Part_2/Practice/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState, useEffect} from 'react'
-
 import Note from './components/Note'
 import noteService from './services/notes'
+import Notification from './components/Notification'
+
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
 
   useEffect(() => {
     noteService
@@ -27,10 +29,10 @@ const App = () => {
       setNotes(notes.map(note => note.id === id ? returnedNote : note))
       })
       .catch(error =>{
-        alert(
-          `the note '${note.content}' was already deleted from server`
-        )
-        console.log(error)
+        setErrorMessage(`Note '${note.content}' was already removed from server`)
+        setTimeout(()=> {
+          setErrorMessage(null)
+        },5000)
         setNotes(notes.filter(n => n.id !== id))
       })
 
@@ -63,6 +65,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage}/>
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important': 'all'}
