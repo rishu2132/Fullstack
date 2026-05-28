9fa44a46d3abca0e06ed56c28cc4d4a3efff687f
Utkarsh <utk1298@gmail.com>
2026-05-28 13:06:07 +0530

    Exercise 3.4

--- Part_3/Exercise/Phonebook_backend/index.js
+++ Part_3/Exercise/Phonebook_backend/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.use(express.json())
-const persons = [
+let persons = [
     { 
       "id": "1",
       "name": "Arto Hellas", 
@@ -44,6 +44,13 @@ app.get('/api/persons/:id', (request,response) => {
         response.statusMessage="content not found"
         response.status(404).end()
     }
+})
+
+app.delete('/api/persons/:id',( request,response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
 })
 
 const PORT = 3001
