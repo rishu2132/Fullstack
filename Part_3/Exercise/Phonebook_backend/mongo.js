9020a088c340eff51cc5e88c0a2a9b31d4abde60
Utkarsh <utk1298@gmail.com>
2026-06-02 20:34:34 +0530

    save phonenumber in databse

---
+++ Part_3/Exercise/Phonebook_backend/mongo.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3){
+    console.log("please enter the password")
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://Utkfullstack:${password}@cluster0.hfieplm.mongodb.net/PhonebookApp?appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url,{family:4})
+
+const numberSchema = new mongoose.Schema({
+    name: String,
+    number:String,
+})
+
+const PhoneNumber = mongoose.model('PhoneNumber', numberSchema)
+
+if (process.argv.length > 3){
+
+    const person = new PhoneNumber({
+        name: process.argv[3],
+        number:process.argv[4],
+    })
+
+    person.save().then(result => {
+        console.log(`added ${person.name} number ${person.number} to phonebook`)
+        mongoose.connection.close()
+    })
+
+}
+
+
+
