1f0c928ec83b30d6bd2761b37efdfb29d2d135b9
Utkarsh <utk1298@gmail.com>
2026-06-03 09:52:08 +0530

    unit 3 c

--- Part_3/backend/index.js
+++ Part_3/backend/index.js
@@ -1,32 +1,22 @@
+require('dotenv').config()
 const express = require('express')
-const mongoose = require('mongoose')
+const Note = require('./models/note')
 
 const app = express()
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
 
 app.use(express.json())
 app.use(express.static('dist'))
 
-const password = process.argv[2]
-
-const url = `mongodb+srv://Utkfullstack:${password}@cluster0.hfieplm.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
-
-mongoose.set('strictQuery',false)
-mongoose.connect(url, {family:4})
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
-
-noteSchema.set('toJSON',{
-  transform:(document,returnedObject)=>{
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Note = mongoose.model('Note',noteSchema)
 
 let notes = [
   {
@@ -61,15 +51,9 @@ app.get('/api/notes',(request, response ) => {
 })
 
 app.get('/api/notes/:id', (request, response ) => {
-    const id = request.params.id
-    const note = notes.find(note => note.id === id)
-    
-    if (note){
-        response.json(note)
-    }
-    else {
-        response.statusMessage ="Data not found"
-        response.status(404).end()}
+   Note.findById(request.params.id).then(note => {
+    response.json(note)
+   })
 })
 
 
@@ -80,14 +64,7 @@ app.delete('/api/notes/:id', (request,response ) => {
     response.status(204).end()
 })
 
-// !   function to generateId
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)
-}
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
@@ -98,18 +75,24 @@ app.post('/api/notes', (request, response) => {
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
-const  PORT = process.env.PORT || 3001
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+
+const  PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
