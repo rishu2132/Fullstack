0e9d94a8285394db49c11b9af9959495f9060e7f
Utkarsh <utk1298@gmail.com>
2026-06-03 16:16:27 +0530

    Exercise 3.14 initial commit

--- Part_3/Exercise/Phonebook_backend/index.js
+++ Part_3/Exercise/Phonebook_backend/index.js
@@ -134,7 +134,7 @@ app.post('/api/persons',(request, response) => {
     
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
 })
