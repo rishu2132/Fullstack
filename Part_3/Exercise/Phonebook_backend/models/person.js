2c308288ade958fa2143a10dcd869b85f5296d47
Utkarsh <utk1298@gmail.com>
2026-06-04 13:41:32 +0530

    Exercise 3.20* validation for number

--- Part_3/Exercise/Phonebook_backend/models/person.js
+++ Part_3/Exercise/Phonebook_backend/models/person.js
@@ -19,7 +19,17 @@ const numberSchema = new mongoose.Schema({
         minLength: 3,
         required: true
     },
-    number:String,
+    number:{
+        type: String,
+        minLength: 8,
+        validate: {
+            validator: function(v){
+                return /\d{2}-\d{6}/.test(v) || /\d{3}-\d{5}/.test(v)
+            },
+            message: props => `${props.value} is not a valid number !`
+        },
+        required: [true,'User phone number required']
+    },
 })
 
 numberSchema.set('toJSON',{
