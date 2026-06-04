f71667f5c3cf5986421ef14f0af728dce5c77f65
Utkarsh <utk1298@gmail.com>
2026-06-04 12:38:40 +0530

    add validation

--- Part_3/backend/index.js
+++ Part_3/backend/index.js
@@ -72,12 +72,6 @@ app.put('/api/notes/:id',(request,response,next) => {
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
-  }
-
   const note = new Note({
     content: body.content,
     important: body.important || false,
@@ -100,7 +94,9 @@ const errorHandler = (error,request,response,next)=> {
 
   if (error.name === 'CastError'){
     return response.status(400).send({error: 'malformatted id'})
-  }
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({error: error.message})
+  } 
 
   next(error)
 }
