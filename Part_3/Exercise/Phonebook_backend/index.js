631e4a4ce28419a4c833fa86e7453516c5b32d14
Utkarsh <utk1298@gmail.com>
2026-06-04 13:20:03 +0530

    Exercise 3.19*

--- Part_3/Exercise/Phonebook_backend/index.js
+++ Part_3/Exercise/Phonebook_backend/index.js
@@ -103,11 +103,6 @@ app.put('/api/persons/:id',(request,response,next) => {
 app.post('/api/persons',(request, response ,next)  => {
     const body = request.body
 
-    if (!body.name || !body.number) {
-        return response.status(400).json({ 
-        error: 'content missing' 
-        })
-    }
 
     const person = new PhoneNumber({
         name:body.name,
@@ -140,6 +135,8 @@ const errorHandler = (error, request, response, next) => {
 
     if(error.name === 'CastError'){
         return response.status(400).send({error: "misformatted id"})
+    } else if (error.name === 'ValidationError'){
+        return response.status(400).json({error: error.message})
     }
 
     next(error)
--- Part_3/Exercise/Phonebook_backend/models/person.js
+++ Part_3/Exercise/Phonebook_backend/models/person.js
@@ -14,7 +14,11 @@ mongoose.connect(url,{family:4})
     })
 
 const numberSchema = new mongoose.Schema({
-    name:String,
+    name:{
+        type: String,
+        minLength: 3,
+        required: true
+    },
     number:String,
 })
 
