9bc0a669d8a2f7dfc323298d19f6046ca22ac88a
Utkarsh <utk1298@gmail.com>
2026-06-07 21:20:36 +0530

    Exercise 4.16

--- Part_4/Blog_list/models/user.js
+++ Part_4/Blog_list/models/user.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    username: String,
-    name: String,
-    passwordHash:String,
+    username: {
+        type: String,
+        required: true,
+        minLength: 3,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true,
+        minLength: 3
+    },
+    passwordHash: String,
     blogs: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Blog'
