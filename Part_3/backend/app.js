1740c477db9b4458bb7b2918217e48b39ca8f3f3
Utkarsh <utk1298@gmail.com>
2026-06-07 13:00:01 +0530

    token authentication for user

--- Part_3/backend/app.js
+++ Part_3/backend/app.js
@@ -5,6 +5,7 @@ const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const notesRouter = require('./controllers/notes')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const app = express()
 logger.info('connecting to', config.MONGODB_URI)
@@ -22,6 +23,7 @@ app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+app.use('/api/login', loginRouter)
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
 
