0d8c49a0a9158a917b7d337ca1eafe6e6bb04017
Utkarsh <utk1298@gmail.com>
2026-06-07 13:42:02 +0530

    use token to verify user while posting notes

--- Part_3/backend/controllers/notes.js
+++ Part_3/backend/controllers/notes.js
@@ -1,6 +1,9 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+
 
 notesRouter.get('/', async (request, response) => {
   const notes = await Note.find({}).populate('user', {username:1, name:1})
@@ -19,9 +22,22 @@ notesRouter.get('/:id',async (request, response, next) => {
     
 })
 
-notesRouter.post('/', async (request, response, next) => {
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if(authorization && authorization.startsWith('Bearer ')){
+    return authorization.replace('Bearer ','')
+  }
+  return null
+}
+
+notesRouter.post('/', async (request, response) => {
   const body = request.body
-  const user = await User.findById(body.userId)
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if(!decodedToken.id){
+    return response.status(401).json({error: 'error invalid'})
+  }
+
+  const user = await User.findById(decodedToken.id)
   
 
   if(!user){
