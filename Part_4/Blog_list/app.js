be65ad24084acae9428c68df6829f7c9c5df6f4f
Utkarsh <utk1298@gmail.com>
2026-06-07 20:32:53 +0530

    Exercise 4.15 part-a create users

--- Part_4/Blog_list/app.js
+++ Part_4/Blog_list/app.js
@@ -4,10 +4,10 @@ const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const blogRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 const app = express()
 logger.info('connecting to database')
-
 
 mongoose.connect(config.MONGODB_URI,{family:4})
     .then(() => {
@@ -20,6 +20,7 @@ app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+app.use('/api/users',usersRouter)
 app.use('/api/blogs',blogRouter)
 
 app.use(middleware.unknownEndpoint)
