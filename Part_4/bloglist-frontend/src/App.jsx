ef8fc943a1343419d9d0a3abeb9a3e401d8d935f
Utkarsh <utk1298@gmail.com>
2026-06-09 09:56:47 +0530

    add login form

--- Part_4/bloglist-frontend/src/App.jsx
+++ Part_4/bloglist-frontend/src/App.jsx
@@ -1,9 +1,12 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, use } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,6 +14,40 @@ const App = () => {
     )  
   }, [])
 
+  const handleLogin = (event) => {
+    event.preventDefault()
+    console.log('logging in ', username,password)
+  }
+
+  if (user === null){
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            <label>username
+            <input 
+              type="text" 
+              value = {username}
+              onChange = {({target}) => setUsername(target.value)}
+              />
+          </label>
+          </div>
+          <div>
+            <label >
+            password
+            <input
+              type="text" 
+              value= {password}
+              onChange={({target}) => setPassword(target.value)}
+              />
+          </label>
+          </div>
+          <button type='submit'>login</button>
+        </form>
+      </div>
+    )
+  }
   return (
     <div>
       <h2>blogs</h2>
