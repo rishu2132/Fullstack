5b83d9ce0dc753f2211d634e57e99665db942cfb
Utkarsh <utk1298@gmail.com>
2026-06-10 16:34:01 +0530

    control the state of database

--- Part_3/backend/app.js
+++ Part_3/backend/app.js
@@ -24,8 +24,15 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/login', loginRouter)
-app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/notes', notesRouter)
+
+if(process.env.NODE_ENV === 'test'){
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
+
+
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
