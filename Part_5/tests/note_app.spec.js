1e5711db63c774d655b6c45cdf7baef892a92715
Utkarsh <utk1298@gmail.com>
2026-06-10 16:00:04 +0530

    write in form during test

--- Part_5/tests/note_app.spec.js
+++ Part_5/tests/note_app.spec.js
@@ -9,4 +9,16 @@ describe('Note app', () => {
     await expect(locator).toBeVisible()
     await expect(page.getByText('Note app, Department of Computer Science, University of Helsinki 2025')).toBeVisible()
   })
+
+  test('user can log in', async ({ page }) => {
+    await page.goto('http://localhost:5173')
+
+    await page.getByRole('button', { name: 'login' }).click()
+    await page.getByLabel('username').fill('root')
+    await page.getByLabel('password').fill('salainen')
+
+    await page.getByRole('button', { name: 'login' }).click()
+
+    await expect(page.getByText('Superuser logged in')).toBeVisible()
+  })
 })
