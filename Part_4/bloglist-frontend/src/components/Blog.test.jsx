bf7e12e7e159201a4641903c82fde9911972679c
Utkarsh <utk1298@gmail.com>
2026-06-12 09:30:12 +0530

    Exercise 5.27

--- Part_4/bloglist-frontend/src/components/Blog.test.jsx
+++ Part_4/bloglist-frontend/src/components/Blog.test.jsx
@@ -3,11 +3,17 @@ import Blog from './Blog'
 import userEvent from '@testing-library/user-event'
 import { MemoryRouter } from 'react-router-dom'
 
+
 const blog = {
   title: 'a new day',
   author: 'rowling',
   url: 'https dfdak as there is of',
-  likes:56
+  likes:56,
+  user: {
+    username: 'mluukkai',
+    name: 'root',
+    password: 'salainen'
+  }
 }
 
 const user = {
@@ -26,13 +32,17 @@ test('blog info and likes are shown to unauthenticated users' , () => {
   expect(screen.queryByText('like')).toBeNull()
 })
 
-test('Authenticated user are shown like button', async () => {
+test('Authenticated user are shown like button', () => {
   render(<MemoryRouter><Blog  blog={blog} user={user}/></MemoryRouter>)
   expect(screen.getByText(blog.url)).toBeDefined()
   expect(screen.getByRole('button',{ name:'like' })).toBeDefined()
 
 })
 
+test('blog creater is shown remove button', () => {
+  render(<MemoryRouter><Blog blog={blog} user={user}/></MemoryRouter>)
+  expect(screen.getByRole('button',{ name:'remove' })).toBeVisible()
+})
 // test('like button is cliked twice', async () => {
 //   const mockHandler = vi.fn()
 
