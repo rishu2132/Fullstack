292b345fa65f5cebc6a5e1aa06d02e5fe67b68cb
Utkarsh <utk1298@gmail.com>
2026-06-12 09:20:15 +0530

    test for blog info and no of likes displayed

--- Part_4/bloglist-frontend/src/components/Blog.test.jsx
+++ Part_4/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,6 +1,7 @@
 import { render , screen } from '@testing-library/react'
 import Blog from './Blog'
 import userEvent from '@testing-library/user-event'
+import { MemoryRouter } from 'react-router-dom'
 
 const blog = {
   title: 'a new day',
@@ -11,32 +12,33 @@ const blog = {
 
 test('renders title and author only' , () => {
 
-  render(<Blog blog={blog}/>)
-  const title = screen.getByText('a new day rowling')
+  render(<MemoryRouter><Blog blog={blog}/></MemoryRouter>)
+  const title = screen.queryByText('rowling: a new day')
   expect(title).toBeDefined()
-  expect(screen.queryByText(blog.url)).toBeNull()
-  expect(screen.queryByText(`likes ${blog.likes}`)).toBeNull()
+  expect(screen.queryByText(blog.url)).toBeVisible()
+  expect(screen.queryByText(`likes ${blog.likes}`)).toBeVisible()
+  expect(screen.queryByText('like')).toBeNull()
 })
 
-test('blog url and likes are shown when button is clicked', async () => {
-  const user = userEvent.setup()
+// test('blog url and likes are shown when button is clicked', async () => {
+//   const user = userEvent.setup()
 
-  render(<Blog  blog={blog}/>)
-  const button = screen.getByText('view')
-  await user.click(button)
-  expect(screen.getByText(blog.url)).toBeDefined()
-  expect(screen.queryByText(`Likes ${blog.likes}`)).toBeDefined()
+//   render(<Blog  blog={blog}/>)
+//   const button = screen.getByText('view')
+//   await user.click(button)
+//   expect(screen.getByText(blog.url)).toBeDefined()
+//   expect(screen.queryByText(`Likes ${blog.likes}`)).toBeDefined()
 
-})
+// })
 
-test('like button is cliked twice', async () => {
-  const mockHandler = vi.fn()
+// test('like button is cliked twice', async () => {
+//   const mockHandler = vi.fn()
 
-  render(<Blog blog={blog} updateLike={mockHandler}/>)
-  const user = userEvent.setup()
-  const viewButton = screen.getByText('view')
-  await user.click(viewButton)
-  const likeButton = screen.getByText('like')
-  await user.dblClick(likeButton)
-  expect(mockHandler.mock.calls).toHaveLength(2)
-})
+//   render(<Blog blog={blog} updateLike={mockHandler}/>)
+//   const user = userEvent.setup()
+//   const viewButton = screen.getByText('view')
+//   await user.click(viewButton)
+//   const likeButton = screen.getByText('like')
+//   await user.dblClick(likeButton)
+//   expect(mockHandler.mock.calls).toHaveLength(2)
+// })
