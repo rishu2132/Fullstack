f8208d75a7b33b2c2b60801ef438a4b4efcb3d26
Utkarsh <utk1298@gmail.com>
2026-06-12 19:14:55 +0530

    use the state from diff component

--- Part_6/zustand/src/App.jsx
+++ Part_6/zustand/src/App.jsx
@@ -1,20 +1,13 @@
-import Display from 
+import Display from './Display'
+import Control from './Control'
 
 const App = () => {
-  const counter = useCounterStore(state => state.counter)
-  const increment = useCounterStore(state => state.increment)
-  const decrement = useCounterStore(state => state.decrement)
-  const zero = useCounterStore(state => state.zero)
+
 
   return (
     <div>
-      <div>{counter}</div>
-      <div>
-        <button onClick={increment}>plus</button>
-        <button onClick={decrement}>minus</button>
-        <button onClick={zero}>zero</button>
-      </div>
-      
+      <Display/>
+      <Control/>
     </div>
   )
 }
