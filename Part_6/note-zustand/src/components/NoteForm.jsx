62439e378669bb715f930911fdfa97102ccf2a59
Utkarsh <utk1298@gmail.com>
2026-06-13 12:55:34 +0530

    send data to server

--- Part_6/note-zustand/src/components/NoteForm.jsx
+++ Part_6/note-zustand/src/components/NoteForm.jsx
@@ -1,14 +1,15 @@
 import { useNoteActions } from "../store"
+import noteService from '../services/notes'
 
 const NoteForm = () => {
 
     const { add } = useNoteActions()
-    const generatedId = () => Number((Math.random() * 1000000).toFixed(0))
 
-    const addNote = (e) => {
+    const addNote = async (e) => {
         e.preventDefault()
         const content = e.target.note.value
-        add({id: generatedId(), content, important: false })
+        const newNote = await noteService.createNew(content)
+        add(newNote)
         e.target.reset()
     }
 
--- Part_6/note-zustand/src/services/notes.js
+++ Part_6/note-zustand/src/services/notes.js
@@ -7,7 +7,20 @@ const getAll = async () => {
     }
 
     return await response.json()
-    
 }
 
-export default { getAll }
+const createNew = async (content) => {
+    const response = await fetch(baseUrl, {
+        method: 'POST',
+        header: {'Content-Type': 'application/json'},
+        body: JSON.stringify({content, important:false})
+    })
+
+    if(!response.ok){
+        throw new Error('Failed to create note')
+    }
+
+    return await response.json()
+}
+
+export default { getAll, createNew }
