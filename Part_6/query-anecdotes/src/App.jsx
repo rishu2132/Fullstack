4e3795c45f767bc546f295c10e8f619585cdd53f
Utkarsh <utk1298@gmail.com>
2026-06-14 15:42:10 +0530

    Exercise 6.20

--- Part_6/query-anecdotes/src/App.jsx
+++ Part_6/query-anecdotes/src/App.jsx
@@ -1,13 +1,19 @@
+import { useState } from 'react'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 import { useAnecdotes } from './hooks/useAnecdotes'
+import NotificationContext from './NotificationContext'
 
 const App = () => {
   const { anecdotes, isPending, isError, voteIncrement} = useAnecdotes()
-
+  const [notification, setNotification] = useState(null)
 
   const handleVote = (anecdote) => {
     voteIncrement(anecdote)
+    setNotification(`anedote "${anecdote.content}" voted`)
+    setTimeout(() => {
+      setNotification(null)
+    },5000)
   } 
 
  
@@ -21,22 +27,24 @@ const App = () => {
 
 
   return (
-    <div>
-      <h3>Anecdote app</h3>
+    <NotificationContext.Provider value={{ notification,setNotification }}>
+      <div>
+        <h3>Anecdote app</h3>
 
-      <Notification />
-      <AnecdoteForm />
+        <Notification />
+        <AnecdoteForm />
 
-      {anecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => handleVote(anecdote)}>vote</button>
+        {anecdotes.map((anecdote) => (
+          <div key={anecdote.id}>
+            <div>{anecdote.content}</div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={() => handleVote(anecdote)}>vote</button>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </NotificationContext.Provider>
   )
 }
 
