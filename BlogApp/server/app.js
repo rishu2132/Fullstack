5460ce2e4e7742ec261fa26f86449eab98a127e8
Utkarsh <utk1298@gmail.com>
2026-06-22 09:20:10 +0530

    Exercise 7.7 frontend and backend in same repo

--- BlogApp/server/app.js
+++ BlogApp/server/app.js
@@ -6,6 +6,7 @@ const middleware = require('./utils/middleware')
 const blogRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const path = require('path')
 
 const app = express()
 logger.info('connecting to database')
@@ -17,7 +18,7 @@ mongoose.connect(config.MONGODB_URI,{family:4})
     .catch(error => logger.error('error connecting to database',error.message))
 
 
-app.use(express.static('dist'))
+
 app.use(express.json())
 app.use(middleware.requestLogger)
 app.use(middleware.tokenExtractor)
@@ -25,9 +26,17 @@ app.use(middleware.tokenExtractor)
 app.use('/api/login',loginRouter)
 app.use('/api/users',usersRouter)
 app.use('/api/blogs',middleware.userExtractor,blogRouter)
+
 if(process.env.NODE_ENV === 'test'){
   const testingRouter = require('./controllers/testing')
   app.use('/api/testing', testingRouter)
+}
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'../client/dist')))
+    app.get('/*splat', (req,res) => {
+        res.sendFile(path.join(__dirname,'../client/dist/index.html'))
+    })
 }
 
 app.use(middleware.unknownEndpoint)
--- BlogApp/server/index.js
+++ BlogApp/server/index.js
@@ -3,6 +3,8 @@ const logger = require('./utils/logger')
 const app = require('./app')
 
 
+
+
 app.listen(config.PORT , () => {
     logger.info(`Server is running on PORT ${config.PORT}`)
 })
