1fc50511136f92703dcd73df1697db9b10c19019
Utkarsh <utk1298@gmail.com>
2026-06-24 15:18:46 +0530

    add route for specific entry

--- Part_9/llari_fight_diaries/src/routes/diaries.ts
+++ Part_9/llari_fight_diaries/src/routes/diaries.ts
@@ -9,6 +9,16 @@ router.get('/',(_req, res: Response<NonSensitiveDiaryEntry[]>) => {
     res.send(data);
 });
 
+router.get('/:id',(req,res) => {
+    const diary = diaryService.findById(Number(req.params.id));
+
+    if (diary) {
+        res.send(diary);
+    } else {
+        res.sendStatus(404);
+    }
+})
+
 router.post('/',(_req,res) => {
     res.send('Saving a diary!');
 });
--- Part_9/llari_fight_diaries/src/services/diaryService.ts
+++ Part_9/llari_fight_diaries/src/services/diaryService.ts
@@ -14,6 +14,11 @@ const getNonSensitiveEntries = ():NonSensitiveDiaryEntry[] => {
     }));
 };
 
+const findById = (id:number): DiaryEntry | undefined => {
+    const entry = diaries.find(d => d.id === id);
+    return entry;
+}
+
 const addDiary = () => {
     return null;
 };
@@ -21,5 +26,6 @@ const addDiary = () => {
 export default {
     getEntries,
     addDiary,
-    getNonSensitiveEntries
+    getNonSensitiveEntries,
+    findById
 };
